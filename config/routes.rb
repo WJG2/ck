d0a57b418588e836a237b1ce8b79630939b3fc85
WJG2 <WJG2@Gingles-Air.local>
2013-09-29 14:26:44 -0700

    Update Welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ck::Application.routes.draw do
+  get "welcome/hello"
   get "welcome/bammo"
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
