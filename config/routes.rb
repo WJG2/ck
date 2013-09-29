44837dd3ca5a72ddc2311f392d4ee2df5cc22ce7
WJG2 <WJG2@Gingles-Air.local>
2013-09-29 15:11:51 -0700

    edit root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Ck::Application.routes.draw do
   get "welcome/hello"
-  get "welcome/bammo"
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'welcome/hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
